088fd631212c3f99a7b596e3e50f543f0a79c2c2
BalbuenaOscarE <balbuenaoscar5@gmail.com>
2023-05-21 11:15:35 -0300

    Segunda Actualización

--- proyecto_final.py
+++ proyecto_final.py
@@ -1,9 +1,12 @@
 import random
 import string
+import sqlite3
 
 pregunta = ""
 
 contraseñas = []
+
+print("Hola!! Bienvenidos a nuestro programa generador y encriptador de contraseñas, esperamos que les sea de utilidad!!!")
 
 def generar_contrasena(longitud):
     caracteres = string.ascii_letters + string.digits + string.punctuation
@@ -25,22 +28,23 @@ while pregunta != "NO" :
 
  pregunta = str(input("¿Quiere guardar alguna otra contraseña? (SI/NO) ").upper())
 
+ if pregunta != "SI" and pregunta != "NO" :
+    print("Por favor, introduzca una respuesta válida")
+
+
+conexion = sqlite3.connect('datos.db')
+
+cursor = conexion.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS datos (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    cuenta TEXT,
+                    contrasena_generada INTEGER
+                )''')
+
+cursor.executemany('INSERT INTO datos (cuenta, contrasena_generada) VALUES (?, ?)', contraseñas)
 
 print(contraseñas)
 
-
-
-
-
- 
-
-
-
-    
-  
-
-
-
-
-
-
+conexion.commit()
+conexion.close()
